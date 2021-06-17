76d0362a3f6406d51c5b621b5fa91610d06c0939
NSurawski <nicolesurawski@gmail.com>
2021-06-17 12:27:43 -0400

    Edit website text

--- src/yourdata.js
+++ src/yourdata.js
@@ -28,7 +28,7 @@ export default {
   ],
   //   Header Paragraph
   headerParagraph:
-    "I am a New York based Full Stack Software Engineer, Certified ScrumMaster, and creative with a passion for agile, design and creating technology that makes peoples lives better.",
+    "I am a New York based Full Stack Software Engineer, Certified ScrumMaster, and creative with a passion for agile, user experience and creating technology that makes peoples lives better.",
 
   //Contact Email
   contactEmail: "nicolesurawski@gmail.com",
@@ -166,7 +166,7 @@ export default {
 
   promotionHeading: "Heading",
   promotionPara:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "haven't decided what goes here yet!",
   // End Promotion Section -----------------
 
   //   Contact Section --------------
